b75065d3ce86c2ed6f1a69f3c0795ffed8a4c2a4
ar-siddiqui <abdulraheemsiddiqui@gmail.com>
2024-08-24 22:06:15 -0400

    Add config file

---
+++ scripts/config.py
@@ -0,0 +1,45 @@
+# config.py
+
+# API Base URL
+RIPPLE1D_API_URL = "http://localhost"
+
+# STAC API settings
+STAC_ENDPOINT = ""
+STAC_COLLECTION = "ripple_test_data"
+
+# Ripple settings
+RAS_VERSION = "631"
+DEPTH_INCREMENT = 1
+RESOLUTION = 3.0
+RESOLUTION_UNITS = "Meters"
+TERRAIN_SOURCE_URL = ""
+
+# Payload templates
+PAYLOAD_TEMPLATES = {
+    "extract_submodel": {
+        "source_model_directory": "{source_model_directory}\\{model_key}",
+        "submodel_directory": "{submodels_directory}\\{nwm_reach_id}",
+        "nwm_id": "{nwm_reach_id}",
+    },
+    "create_ras_terrain": {
+        "submodel_directory": "{submodels_directory}\\{nwm_reach_id}",
+        "resolution": RESOLUTION,
+        "resolution_units": RESOLUTION_UNITS,
+        "terrain_source_url": TERRAIN_SOURCE_URL,
+    },
+    "create_model_run_normal_depth": {
+        "submodel_directory": "{submodels_directory}\\{nwm_reach_id}",
+        "plan_suffix": "ind",
+        "num_of_discharges_for_initial_normal_depth_runs": 10,
+        "ras_version": RAS_VERSION,
+    },
+    "run_incremental_normal_depth": {
+        "submodel_directory": "{submodels_directory}\\{nwm_reach_id}",
+        "plan_suffix": "nd",
+        "depth_increment": DEPTH_INCREMENT,
+        "ras_version": RAS_VERSION,
+    },
+}
+
+# Poll wait time for job status checks
+POLL_WAIT = 3
